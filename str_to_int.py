2cd95e47d7a0b63fed5f6b99cf7f671fb7ab76d9
SaiKrishnaBala <saikrishna.bala@gmail.com>
2022-06-06 21:49:09 +0530

    Create str_to_int.py

---
+++ str_to_int.py
@@ -0,0 +1,41 @@
+# String to Integer (atoi)
+# Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+# The algorithm for myAtoi(string s) is as follows:
+
+# Read in and ignore any leading whitespace.
+# Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+# Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+# Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+# If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+# Return the integer as the final result.
+# Note:
+
+# Only the space character ' ' is considered a whitespace character.
+# Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        if len(s) == 0:
+            return 0
+        output = 0
+        index = 0
+        mul_factor = 1
+        while (index < len(s)) and (s[index] == ' '):
+            index += 1
+        if (index < len(s)) and (s[index] == '-' or s[index] == '+'):
+            if s[index] == '-':
+                mul_factor = -1
+            index += 1
+        for index in range(index, len(s)):
+            if (ord(s[index]) >= ord('0')) and (ord(s[index]) <= ord('9')):
+                output = output*10 + int(s[index])
+                print(output)
+            else:
+                break
+        output = output * mul_factor
+        print(output)
+        output = min(output, pow(2, 31)-1)
+        print(output)
+        output = max(output, pow(-2, 31))
+        print(output)
+        return output
